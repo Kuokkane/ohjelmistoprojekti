de38dd6b684f5ccc8a9a11d98f2908ed8ac4158a
Kuokkane <tonikuokkanen@hotmail.com>
2019-03-25 10:42:14 +0200

    harjoitustyön aloitus

---
+++ src/main/java/hh/softala/softalaharjoitus/SoftalaharjoitusApplication.java
@@ -0,0 +1,13 @@
+package hh.softala.softalaharjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftalaharjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftalaharjoitusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/softala/softalaharjoitus/SoftalaharjoitusApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.softala.softalaharjoitus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftalaharjoitusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
