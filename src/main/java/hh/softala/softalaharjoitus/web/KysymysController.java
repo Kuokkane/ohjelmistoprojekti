44e1949a48bce5685b1cb862bdbcd63f3638a6e9
Petra Kohva <petra.kohva@myy.haaga-helia.fi>
2019-04-01 08:39:59 +0300

    controlleri tehty

--- src/main/java/hh/softala/softalaharjoitus/web/KysymysController.java
+++ src/main/java/hh/softala/softalaharjoitus/web/KysymysController.java
@@ -1,17 +1,23 @@
 package hh.softala.softalaharjoitus.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.softala.softalaharjoitus.domain.Kysymys;
+import hh.softala.softalaharjoitus.domain.KysymysRepository;
+
 @Controller
 public class KysymysController {
 	
+	private KysymysRepository krepository;
 	
 	@RequestMapping(value="/kysymykset", method=RequestMethod.GET)
-	public @ResponseBody String kysymyslistaRest(){
-		return "moi";
+	public @ResponseBody List<Kysymys> kysymyslistaRest(){
+		return (List<Kysymys>) krepository.findAll();
 	
 	}
 
