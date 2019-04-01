61433c47187cfa0a336ce1ea80a4a18335678580
Kuokkane <tonikuokkanen@hotmail.com>
2019-04-01 08:31:28 +0300

    KysymysRepository

---
+++ src/main/java/hh/softala/softalaharjoitus/domain/KysymysRepository.java
@@ -0,0 +1,11 @@
+package hh.softala.softalaharjoitus.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
+	
+	List<Kysymys> findKysymysById(long id);
+
+}
