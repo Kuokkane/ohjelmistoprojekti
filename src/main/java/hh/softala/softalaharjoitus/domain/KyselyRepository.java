1d81c430c4934e0442da27ab37e27322e6e841ec
Mari Luostarinen <mari.luostarinen@myy.haaga-helia.fi>
2019-05-13 09:59:44 +0300

    kyselyä

--- src/main/java/hh/softala/softalaharjoitus/domain/KyselyRepository.java
+++ src/main/java/hh/softala/softalaharjoitus/domain/KyselyRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface KyselyRepository extends CrudRepository<Kysely, Long> {
 	
 	List<Kysely> findByNimi(String nimi);
+	List<Kysely> findByKyselyId(Long kyselyId);
 
 }
